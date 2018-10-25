eedd9ea745a4c412978e1318a7890cb4f81b3565
Kevin Fleming <flevinkelming@protonmail.com>
2018-10-25 13:45:44 -0400

    correcting trx addition when > 136_000

--- tronbet_calc.rs
+++ tronbet_calc.rs
@@ -1,43 +1,41 @@
 fn main() {
-    let mut trx = 17_391.0;
-    let ante = 5_319.0;
+    let trx_banked = 0.0;
+    let mut trx = 0.0; // wager period start amount
+    let total_ante = 0.0; // 43_245.9 TRX
     let mut ante_mined = 0.0;
     
     let multiplier = 1.037; // 95% @ 1.037 is ideal; 19/20 bets should win
     let ante_stage = 1_000.0 + (10.0 * 2.0f64.powf(1.0));
-    let wager_per_day = 10;
-    let wager_cycle = 5; // days
+    let max_wager_per_day = 10;
+    let wager_period = 5; // days
+    let tb_max_bet = 135_000.0;
     
-    for wager in 1..(wager_per_day*wager_cycle + 1) {
+    for w in 1..(max_wager_per_day * wager_period + 1) {
+        // TRONbet's max wager amount is around 136_000 TRX
         if trx < 136_000.0 {
             ante_mined += trx / ante_stage;
             trx *= multiplier;
         } else {
-            ante_mined += 135_000.0 / ante_stage;
-            trx += 6_480.0; // max profit per bet @ 135_000 TRX wager
+            ante_mined += tb_max_bet / ante_stage;
+            trx += tb_max_bet * multiplier - tb_max_bet; 
         }
         
-        if wager%wager_per_day == 0 {
+        if w % max_wager_per_day == 0 {
             trx -= 100.0; // With strategy should lose 100 TRX per max bet win
-            let usd_value = fmt_f(trx * 0.023); // 1 TRX = 0.023 Dollars
-            let trx_value = fmt_f(ante_mined * 8.1); // 1 ANTE = 8.1 TRX
+            let usd_value = fmt_f(trx * 0.023); // 1 TRX ~= $0.023
+            let trx_value = fmt_f(ante_mined * 8.1); // 1 ANTE ~= 8.1 TRX
             
-            println!("Day {}\tEnding TRX:\t{} (${})\n \
-                \tANTE Gained:\t{} ({} TRX)\n",
-                wager / wager_per_day,
-                fmt_f(trx),
-                usd_value,
-                fmt_f(ante_mined),
+            println!("Day {}\tEnding TRX:\t{} (${})\n\tANTE Gained:\t{} ({} TRX)\n",
+                w / max_wager_per_day, fmt_f(trx), usd_value, fmt_f(ante_mined),
                 trx_value,
             );
         }
     }
     
-    let total_trx_value = (ante+ante_mined) * 8.1; // dividends
-    let payout = fmt_f(total_trx_value * 0.023);
-    println!("Total TRX dividends payout: {} (${})",
-        fmt_f(total_trx_value),
-        payout,
+    let dividends_payout = fmt_f(((total_ante+ante_mined) * 8.1) * 0.023);
+    println!("Total TRX:\t{}\nTotal ANTE:\t{} (${} per payout)\nUSD Value:\t${}",
+        fmt_f(trx_banked+trx), fmt_f(total_ante+ante_mined), dividends_payout,
+        fmt_f((trx_banked+trx+(total_ante*8.1)+(ante_mined*8.1)) * 0.023),
     );
 }
 
